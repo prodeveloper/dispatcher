cb6ce797bb3469f20545436214bb9b0edf291a1a
Chencha Jacob <jmokua@gmail.com>
2015-01-29 16:32:26 +0300

    Added new listener for live dispatch

---
+++ Chencha/Listeners/LiveListener.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jacob
+ * Date: 29/01/15
+ * Time: 16:23
+ */
+
+namespace Chencha\Listeners;
+
+use Laracasts\Commander\Events\EventListener as L_EventListener;
+class LiveListener  extends L_EventListener {
+
+}
